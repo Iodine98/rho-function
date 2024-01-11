4b98e2db12abdf702c06de1c6c144fe0c87978e9
Rohan Sobha <rohanraysobha@gmail.com>
2024-01-11 12:05:34 +0000

    Rational numbers

--- rho/rho_base.py
+++ rho/rho_base.py
@@ -1,7 +1,9 @@
 from typing import Any
-from tqdm.auto import tqdm
 from decimal import Decimal
-from rho.rho_global import RhoGlobal, RhoTuples
+
+from tqdm.auto import tqdm
+
+from rho.rho_global import RhoGlobal, RhoTuples, count_decimals
 
 
 class RhoBase(RhoGlobal):
@@ -34,7 +36,7 @@ class RhoBase(RhoGlobal):
                 if number == 1 or base > root:
                     break
                 if number < n_input:
-                    rho_bases = self.rho_base(number, base, inc, self.enable_tqdm and self.trace) 
+                    rho_bases = self.rho_base(number, base, inc, self.enable_tqdm and self.trace)
                     pairs.extend(rho_bases)
                     number = 1
                     continue
@@ -47,5 +49,23 @@ class RhoBase(RhoGlobal):
             pairs.append((number, 1))
         range_bar.close()
         return pairs
-    
-    
+
+    def rho_rational_number(self, f_input: float, base: int = 2, inc=1) -> RhoTuples:
+        """
+        Calculates the Rho value and the probability for a given rational number.
+
+        Args:
+            f_input (float): The input rational number.
+            base (int, optional): The base for the Rho calculation. Defaults to 2.
+            inc (int, optional): The increment value for the Rho calculation. Defaults to 1.
+
+        Returns:
+            tuple[RhoTuples, float]: A tuple containing the Rho value and the probability.
+        """
+        no_of_decimals = count_decimals(f_input)
+        numerator = int(round(f_input * 10**no_of_decimals))
+        denominator = 10**no_of_decimals
+        rho_numerator = self.rho_base(numerator, base, inc, self.enable_tqdm)
+        rho_denominator = self.rho_base(denominator, base, inc, self.enable_tqdm)
+        rho = self.rho_union(rho_numerator, self.rho_power(rho_denominator, -1), enable_tqdm=False)
+        return rho
